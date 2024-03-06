8fc4630ec427c63655ea0abeb723d4e016634863
Rian Durigan <durigansrian@gmail.com>
2024-03-06 14:14:07 -0300

    iniciando projeto, criando os projetos navegacao e Atracao

---
+++ src/components/Navegacao/Navegacao.jsx
@@ -0,0 +1,30 @@
+import './Navegacao.css'
+function Navegacao() {
+
+    return (
+        <div className="barra-navegacao">
+
+            <div className="logo-zoo">
+                <h1>ZOOLÓGICO</h1>
+            </div>
+
+
+            <div className="navbar-opcoes">
+                <ul>
+                    <li>Inicio</li>
+                    <li>Os animais</li>
+                    <li>Fotos</li>
+                    <li>Depoimentos</li>
+                    <li>Login</li>
+                </ul>
+            </div>
+
+        </div>
+    );
+
+
+
+
+}
+
+export default Navegacao;
